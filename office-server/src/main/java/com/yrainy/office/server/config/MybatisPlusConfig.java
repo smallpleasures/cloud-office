80d579aaa99e9639a86560d9b606ccd0d13effcf
zhanglun <yrainy2021@163.com>
2022-04-19 17:16:39 +0800

    分页插件设置数据库类型

--- office-server/src/main/java/com/yrainy/office/server/config/MybatisPlusConfig.java
+++ office-server/src/main/java/com/yrainy/office/server/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.yrainy.office.server.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,8 @@ public class MybatisPlusConfig {
 
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+        PaginationInterceptor interceptor = new PaginationInterceptor();
+        interceptor.setDbType(DbType.MYSQL);
+        return interceptor;
     }
 }
